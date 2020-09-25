30388e5f76fbd110d221bbd0f40259c5e7557bea
Prasanth Raghavan <p.raghavan@aximglobal.com>
2020-09-25 14:21:02 +0530

    Added babel for ES6, nodemon, eslint, and prettier

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
 
-router.get('/', function(req, res, next) {
-  return res.status(200).json({ message: 'Todo REST APIs' });
-});
+const indexRouter = express.Router();
 
-module.exports = router;
+indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Todo REST APIs' }));
+
+export default indexRouter;
