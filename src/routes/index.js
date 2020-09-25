b67c96e7f944eec797403c2d8712fb7efde5df22
Prasanth Raghavan <p.raghavan@aximglobal.com>
2020-09-25 14:27:23 +0530

    Added environment variable

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import { testEnvironmentVariable } from '../settings';
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Todo REST APIs' }));
+indexRouter.get('/', (req, res) => res
+  .status(200)
+  .json({ message: `Todo REST APIs | ${testEnvironmentVariable}` }));
 
 export default indexRouter;
